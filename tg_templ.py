899dec4675de5a9099238338069ab9cbdec4e862
Theodor Stana <theodor.stana@bradford-space.com>
2022-12-21 17:13:33 +0100

    Simplify template

--- tg_templ.py
+++ tg_templ.py
@@ -1,27 +1,15 @@
 TEMPL_VHD = """--==============================================================================
--- $long_entity_name
+-- $entity : $design_name
 --==============================================================================
 --
--- author: $name ($mail)
+-- Created on: $date
 --
--- date of creation: $date
+-- Description:
 --
--- description:
---
---      <TODO: Add description...>
+--   <TODO: Add description...>
 --
 --==============================================================================
--- GNU LESSER GENERAL PUBLIC LICENSE
---==============================================================================
--- This source file is free software; you can redistribute it and/or modify it
--- under the terms of the GNU Lesser General Public License as published by the
--- Free Software Foundation; either version 2.1 of the License, or (at your
--- option) any later version. This source is distributed in the hope that it
--- will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
--- of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
--- See the GNU Lesser General Public License for more details. You should have
--- received a copy of the GNU Lesser General Public License along with this
--- source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
+-- Copyright (c) $year $company
 --==============================================================================
 
 library ieee;
@@ -79,136 +67,57 @@ end architecture $arch;
 
 TEMPL_CHDR = """/*
  *==============================================================================
- * $long_entity_name
+ * $design_name
  *
  * $ftype file
  *==============================================================================
  *
- * author: $name ($mail)
+ * Created on: $date
  *
- * date of creation: $date
- *
- * description:
+ * Description:
  *
  *      TODO: Add description here...
  *
  *==============================================================================
- * GNU LESSER GENERAL PUBLIC LICENSE
- *==============================================================================
- * This source file is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version. This source is distributed in the hope that it
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details. You should have
- * received a copy of the GNU Lesser General Public License along with this
- * source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
+ * Copyright (c) $year $company
  *==============================================================================
  */
 
 #ifndef __${entity}_H_
 #define __${entity}_H_
 
-/*
- *==============================================================================
- * <your func name here>
- *
- *  params:
- *     * <your func params here>
- *
- *  returns:
- *    - <your func returns here>
- *
- *  comments:
- *     <your comments here>
- *==============================================================================
- */
-
 #endif // __${entity}_H_
 """
 
 TEMPL_CSRC = """/*
  *==============================================================================
- * $long_entity_name
+ * $design_name
  *
  * $ftype file
  *==============================================================================
  *
- * author: $name ($mail)
+ * Created on: $date
  *
- * date of creation: $date
- *
- * description:
+ * Description:
  *
  *      TODO: Add description here...
  *
  *==============================================================================
- * GNU LESSER GENERAL PUBLIC LICENSE
- *==============================================================================
- * This source file is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version. This source is distributed in the hope that it
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details. You should have
- * received a copy of the GNU Lesser General Public License along with this
- * source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
- *==============================================================================
- */
-
-/*
- *==============================================================================
- * <your func name here>
- *
- *  params:
- *     * <your func params here>
- *
- *  returns:
- *    - <your func returns here>
- *
- *  comments:
- *     <your comments here>
+ * Copyright (c) $year $company
  *==============================================================================
  */
 """
 
 TEMPL_PY = """#===============================================================================
-# $long_entity_name
+# $design_name
 #===============================================================================
-# author: $name ($mail)
+# Created on: $date
 #
-# date of creation: $date
-#
-# description:
+# Description:
 #
 #       TODO: Add description here...
 #
 #===============================================================================
-# GNU LESSER GENERAL PUBLIC LICENSE
-#===============================================================================
-# This source file is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by the
-# Free Software Foundation; either version 2.1 of the License, or (at your
-# option) any later version. This source is distributed in the hope that it
-# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Lesser General Public License for more details. You should have
-# received a copy of the GNU Lesser General Public License along with this
-# source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
-#===============================================================================
-
-#===============================================================================
-# <your func name here>
-#
-#   params:
-#     * <your func params here>
-#
-#   returns:
-#     * <your func returns here>
-#
-#   comments:
-#     <your comments here>
+# Copyright (c) $year $company
 #===============================================================================
 """
